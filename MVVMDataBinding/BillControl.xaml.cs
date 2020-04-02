405f433f3c28956133bb773c7bb9e06c5308ec53
Blake Davies <bbd@ksu.edu>
2020-04-02 12:12:42 -0500

    Followed the right up sir

---
+++ MVVMDataBinding/BillControl.xaml.cs
@@ -0,0 +1,82 @@
+﻿using CashRegister;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MVVMDataBinding
+{
+    /// <summary>
+    /// Interaction logic for BillControl.xaml
+    /// </summary>
+    public partial class BillControl : UserControl
+    {
+
+        public static readonly DependencyProperty DenominationProperty =
+            DependencyProperty.Register(
+                "Denomination",
+                typeof(Bills),
+                typeof(BillControl),
+                new PropertyMetadata(Bills.One)
+                );
+
+
+        public Bills Denomination
+        {
+            get => (Bills)GetValue(DenominationProperty);
+            set { SetValue(DenominationProperty, value); }
+        }
+
+        public static DependencyProperty QuantityProperty =
+            DependencyProperty.Register(
+                "Quantity",
+                typeof(int),
+                typeof(BillControl),
+                new FrameworkPropertyMetadata(0, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault)
+                );
+
+
+        public int Quantity
+        {
+            get => (int)GetValue(QuantityProperty);
+            set { SetValue(QuantityProperty, value); }
+        }
+
+
+
+        public BillControl()
+        {
+            InitializeComponent();
+        }
+
+
+        /// <summary>
+        /// Increases the quantity of the bound coinage by one
+        /// </summary>
+        /// <param name="sender">The coinage quanity (as an int)</param>
+        /// <param name="args">The event args</param>
+        public void OnIncreaseClicked(object sender, RoutedEventArgs args)
+        {
+            Quantity++;
+        }
+
+        /// <summary>
+        /// Decreases the quantity of the bound coinage by one
+        /// </summary>
+        /// <param name="sender">The coinage quantity (as in int)</param>
+        /// <param name="args">The event args</param>
+        public void OnDecreaseClicked(object sender, RoutedEventArgs args)
+        {
+            Quantity--;
+        }
+
+    }
+}
---
+++ MVVMDataBinding/CashResgisterModelView.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+using CashRegister;
+using System.Windows.Navigation;
+using System.Windows.Controls;
+
+namespace MVVMDataBinding
+{
+    public class CashResgisterModelView : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// notifies when changed
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// the drarwer backing
+        /// </summary>
+        private CashDrawer drawer = new CashDrawer();
+
+        /// <summary>
+        /// total value
+        /// </summary>
+        public double TotalValue => drawer.TotalValue;
+
+        /// <summary>
+        /// pennies getter and setter
+        /// </summary>
+        public int Pennies
+        {
+            get => drawer.Pennies;
+
+            set
+            {
+                if (drawer.Pennies == value || value < 0) return;
+                int quantity = value - drawer.Pennies;
+                if (quantity > 0) drawer.AddCoin(Coins.Penny, quantity);
+                else drawer.RemoveCoin(Coins.Penny, -quantity);
+                invokePropertyChanged("Pennies");
+            }
+        }
+
+        public int Nickels
+        {
+            get => drawer.Nickels;
+            set
+            {
+                if (drawer.Nickels == value || value < 0) return;
+                int quantaty = value - drawer.Nickels;
+                if (quantaty > 0)
+                {
+                    drawer.AddCoin(Coins.Nickel, quantaty);
+                }
+                else
+                {
+                    drawer.RemoveCoin(Coins.Nickel, -quantaty);
+                }
+                invokePropertyChanged("Nickels");
+            }
+
+        }
+
+        
+        public int Dimes
+        {
+            get => drawer.Dimes;
+            set
+            {
+                if (drawer.Dimes == value || value < 0) return;
+                int quantaty = value - drawer.Dimes;
+                if (quantaty > 0)
+                {
+                    drawer.AddCoin(Coins.Dime, quantaty);
+                }
+                else
+                {
+                    drawer.RemoveCoin(Coins.Dime, -quantaty);
+                }
+                invokePropertyChanged("Dimes");
+            }
+
+        }
+                
+        public int Quarters
+        {
+            get => drawer.Quarters;
+            set
+            {
+                if (value == drawer.Quarters || value < 0) return;
+                int quantaty = value - drawer.Quarters;
+                if (quantaty > 0)
+                {
+                    drawer.AddCoin(Coins.Quarter, quantaty);
+                }
+                else
+                {
+                    drawer.RemoveCoin(Coins.Quarter, -quantaty);
+                }
+                invokePropertyChanged("Quarters");
+            }
+
+        }
+        
+        public int HalfDollars
+        {
+            get => drawer.HalfDollars;
+
+            set
+            {
+                if (drawer.HalfDollars == value || value < 0) return;
+                int quantity = value - drawer.HalfDollars;
+                if (quantity > 0) drawer.AddCoin(Coins.HalfDollar, quantity);
+                else drawer.RemoveCoin(Coins.HalfDollar, -quantity);
+                invokePropertyChanged("HalfDollars");
+            }
+
+        }
+        public int Dollars
+        {
+            get => drawer.Dollars;
+            set
+            {
+                if (value == drawer.Dollars || value < 0) return;
+                int quantaty = value - drawer.Dollars;
+                if (quantaty > 0)
+                {
+                    drawer.AddCoin(Coins.Dollar, quantaty);
+                }
+                else
+                {
+                    drawer.RemoveCoin(Coins.Dollar, -quantaty);
+                }
+                invokePropertyChanged("Dollars");
+            }
+
+        }
+        
+        public int Ones
+        {
+            get => drawer.Ones;
+            set
+            {
+                if (value == drawer.Ones || value < 0) return;
+                int quantaty = value - drawer.Ones;
+                if (quantaty > 0)
+                {
+                    drawer.AddBill(Bills.One, quantaty);
+                }
+                else
+                {
+                    drawer.AddBill(Bills.One, -quantaty);
+                }
+                invokePropertyChanged("Ones");
+            }
+
+        }
+             
+        public int Twos
+        {
+            get => drawer.Twos;
+            set
+            {
+                if (value == drawer.Twos || value < 0) return;
+                int quantaty = value - drawer.Twos;
+                if (quantaty > 0)
+                {
+                    drawer.AddBill(Bills.Two, quantaty);
+                }
+                else
+                {
+                    drawer.AddBill(Bills.Two, -quantaty);
+                }
+                invokePropertyChanged("Twos");
+            }
+
+        }
+                     
+        public int Fives
+        {
+            get => drawer.Fives;
+            set
+            {
+                if (value == drawer.Fives || value < 0) return;
+                int quantaty = value - drawer.Fives;
+                if (quantaty > 0)
+                {
+                    drawer.AddBill(Bills.Five, quantaty);
+                }
+                else
+                {
+                    drawer.AddBill(Bills.Five, -quantaty);
+                }
+                invokePropertyChanged("Five");
+            }
+
+        }
+                             
+        public int Tens
+        {
+            get => drawer.Tens;
+            set
+            {
+                if (value == drawer.Tens || value < 0) return;
+                int quantaty = value - drawer.Tens;
+                if (quantaty > 0)
+                {
+                    drawer.AddBill(Bills.Ten, quantaty);
+                }
+                else
+                {
+                    drawer.AddBill(Bills.Ten, -quantaty);
+                }
+                invokePropertyChanged("Tens");
+            }
+
+        }                             
+        public int Twenties
+        {
+            get => drawer.Twenties;
+            set
+            {
+                if (value == drawer.Twenties || value < 0) return;
+                int quantaty = value - drawer.Twenties;
+                if (quantaty > 0)
+                {
+                    drawer.AddBill(Bills.Twenty, quantaty);
+                }
+                else
+                {
+                    drawer.AddBill(Bills.Twenty, -quantaty);
+                }
+                invokePropertyChanged("Twenties");
+            }
+
+        }                             
+        public int Fifties
+        {
+            get => drawer.Fifties;
+            set
+            {
+                if (value == drawer.Fifties || value < 0) return;
+                int quantaty = value - drawer.Fifties;
+                if (quantaty > 0)
+                {
+                    drawer.AddBill(Bills.Fifty, quantaty);
+                }
+                else
+                {
+                    drawer.AddBill(Bills.Fifty, -quantaty);
+                }
+                invokePropertyChanged("Fifties");
+            }
+
+        }                             
+        public int Hundreds
+        {
+            get => drawer.Hundreds;
+            set
+            {
+                if (value == drawer.Hundreds || value < 0) return;
+                int quantaty = value - drawer.Hundreds;
+                if (quantaty > 0)
+                {
+                    drawer.AddBill(Bills.Hundred, quantaty);
+                }
+                else
+                {
+                    drawer.AddBill(Bills.Hundred, -quantaty);
+                }
+                invokePropertyChanged("Hundreds");
+            }
+
+        }
+
+        /// <summary>
+        /// helper for property changed invoke
+        /// </summary>
+        /// <param name="changed"></param>
+        private void invokePropertyChanged(string changed)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(changed));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("TotalValue"));
+        }
+
+    }
+}
